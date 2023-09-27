c68c6180d14658817554c1a54321f59fd07118f2
Omachonu Prosper <omachonucodes@gmail.com>
2023-09-27 09:46:00 +0100

    Fix indexing issues

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def shipping_freight_cost(products, shipment_price):
     Workflow:
     - Get the product with the highest weight
     - Reduce the quantity of that product by 1
+        We reduce the quatnity of the highest product by one so
+        when we multiply by 2 we wouldnt be adding the max weight product
+        in the calculations again
     - Calculate the total weight
     - Compute the total cost (total weight * shipment price)
 
@@ -32,10 +35,11 @@ def shipping_freight_cost(products, shipment_price):
             product['weight'] = 1
             weight = 1
 
-        if weight > max_weight_product['weight']:
+        # The workflow section of the docstring explains this section in more details
+        if weight >= max_weight_product['weight']:
             max_weight_product['weight'] = weight
             index = max_weight_product['index_in_products']
-            if index:
+            if index is not None:
                 products[index]['quantity'] = int(products[index]['quantity']) + 1
             
             product['quantity'] = int(product['quantity']) - 1
@@ -57,8 +61,8 @@ if __name__ == '__main__':
     ]
 
     products2 = [
-        {"weight":"1", "quantity":"3", "naira_price": "3200"},
-        {"weight":"0.5", "quantity":"2", "naira_price": "2100"}
+        {"weight":"0.5", "quantity":"2", "naira_price": "2100"},
+        {"weight":"1", "quantity":"3", "naira_price": "3200"}
     ]
 
     products3 = [
@@ -66,7 +70,7 @@ if __name__ == '__main__':
     ]
 
     products4 = [
-        {"weight":"0.5", "quantity":"2", "naira_price": "3200"}
+        {"weight":"0.5", "quantity":"3", "naira_price": "3200"}
     ]
     
     print(shipping_freight_cost(products1, 10))
